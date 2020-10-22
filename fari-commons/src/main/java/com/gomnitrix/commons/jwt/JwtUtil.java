f9ff5ca241f3213e3485d3fe8b2d0e6507d3c778
gomnitrix <934196769@qq.com>
2020-10-22 18:05:45 +0800

    add JwtUtil Test

--- fari-commons/src/main/java/com/gomnitrix/commons/jwt/JwtUtil.java
+++ fari-commons/src/main/java/com/gomnitrix/commons/jwt/JwtUtil.java
@@ -9,25 +9,41 @@ import lombok.extern.slf4j.Slf4j;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Slf4j
 public class JwtUtil {
+    private static Key secretKey;
+    private static String issuer;
+    private static Integer expiresSecond;
+    static {
+        Properties properties = new Properties();
+        InputStream in = JwtUtil.class.getResourceAsStream("/jwt.properties");
+        try {
+            properties.load(in);
+            secretKey = generateKey(properties.getProperty("base64Secret").trim());
+            issuer = properties.getProperty("issuer").trim();
+            expiresSecond = Integer.parseInt(properties.getProperty("expiresSecond").trim());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static Key generateKey(String secretKey) {
         byte[] encodedKey = DatatypeConverter.parseBase64Binary(secretKey);
         return new SecretKeySpec(encodedKey, SignatureAlgorithm.HS256.getJcaName());
     }
 
-    public static String createJWT(String userId, String userName, String role, Audience audience) {
+    public static String createJWT(String userId, String userName, String role, String audience) {
         // 生成JWT的时间
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
-
-        Key key = generateKey(audience.getBase64Secret());
 
         String encryUserId = Base64Util.base64Encode(userId); //userID加密
         Map<String, Object> claims = new HashMap<>();
@@ -38,11 +54,12 @@ public class JwtUtil {
                 // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setIssuer(audience.getIssuer())
+                .setIssuer(issuer)
                 .setSubject(userName)
-                .signWith(key);
+                .setAudience(audience)
+                .signWith(secretKey);
         // 设置过期时间
-        int ttlMillis = audience.getExpiresSecond();
+        int ttlMillis = expiresSecond;
         if (ttlMillis >= 0) {
             long expMillis = nowMillis + ttlMillis;
             Date exp = new Date(expMillis);
@@ -58,9 +75,9 @@ public class JwtUtil {
      * @param jwt
      * @return
      */
-    private static Claims parseJWT(String jwt, String base64Security) {
+    private static Claims parseJWT(String jwt) {
         return Jwts.parserBuilder()
-                            .setSigningKey(generateKey(base64Security))
+                            .setSigningKey(secretKey)
                             .build()
                             .parseClaimsJws(jwt)
                             .getBody();
@@ -69,32 +86,33 @@ public class JwtUtil {
     /**
      * get UserName from jwt
      * @param jwt
-     * @param base64Security
      * @return
      */
-    public static String getUserName(String jwt, String base64Security){
-        return parseJWT(jwt, base64Security).getSubject();
+    public static String getUserName(String jwt){
+        return parseJWT(jwt).getSubject();
     }
 
     /**
      * get plain userId from jwt
      * @param jwt
-     * @param base64Security
      * @return
      */
-    public static String getUserId(String jwt, String base64Security){
-        String cipherUserId = parseJWT(jwt, base64Security).get("userId", String.class);
+    public static String getUserId(String jwt){
+        String cipherUserId = parseJWT(jwt).get("uid", String.class);
         return Base64Util.base64Decode(cipherUserId);
+    }
+
+    public static String getAudience(String jwt){
+        return parseJWT(jwt).getAudience();
     }
 
     /**
      * check if token expired
      * @param jwt
-     * @param base64Security
      * @return
      */
-    public static Boolean isExpiration(String jwt, String base64Security) {
-        return parseJWT(jwt, base64Security).getExpiration().before(new Date());
+    public static Boolean isExpiration(String jwt) {
+        return parseJWT(jwt).getExpiration().before(new Date());
     }
 
     public static Boolean isExpiration(Claims claims){
@@ -106,8 +124,8 @@ public class JwtUtil {
      *
      * @return
      */
-    public static boolean checkToken(String jwt, Audience audience) throws Exception {
-        Claims claims = JwtUtil.parseJWT(jwt, audience.getBase64Secret());
+    public static boolean checkToken(String jwt) throws Exception {
+        Claims claims = JwtUtil.parseJWT(jwt);
         return isExpiration(claims);
     }
 }
---
+++ fari-commons/src/test/java/com/gomnitrix/commons/jwt/JwtUtilTest.java
@@ -0,0 +1,38 @@
+package com.gomnitrix.commons.jwt;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class JwtUtilTest {
+    private static final String userId = "testID";
+    private static final String userName = "testName";
+    private static final String role = "testRole";
+    private static final String audience = "testAudience";
+    private static final String testJwt = JwtUtil.createJWT(userId, userName, role, audience);
+
+//    @Test
+//    void createJWT() {
+//    }
+
+    @Test
+    void getUserName() {
+        Assert.assertEquals(userName, JwtUtil.getUserName(testJwt));
+    }
+
+    @Test
+    void getUserId() {
+        Assert.assertEquals(userId, JwtUtil.getUserId(testJwt));
+    }
+
+    @Test
+    void isExpiration() {
+        String testJwt2 = JwtUtil.createJWT(userId, userName, role, audience);
+        Assert.assertEquals(Boolean.FALSE, JwtUtil.isExpiration(testJwt2));
+    }
+
+    @Test
+    void checkToken() {
+        String testJwt2 = JwtUtil.createJWT(userId, userName, role, audience);
+        Assert.assertEquals(Boolean.FALSE, JwtUtil.isExpiration(testJwt2));
+    }
+}
