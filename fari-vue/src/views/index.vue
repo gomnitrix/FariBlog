aeaee3e57de8edc2e0eb5118cc56a0655393c7f1
gomnitrix <934196769@qq.com>
2020-11-07 23:59:44 +0800

    fix logo

--- fari-vue/src/views/index.vue
+++ fari-vue/src/views/index.vue
@@ -44,7 +44,7 @@ export default {
   data () {
     return {
       showLoignBox: false,
-      imgSrc: require('@/assets/logo.png')
+      imgSrc: require('@/assets/FariBlog.jpg')
     }
   },
   methods: {
