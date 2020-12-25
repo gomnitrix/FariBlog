e1e23181596551c2007191f8ff7bdb7dda22b6ac
gomnitrix <934196769@qq.com>
2020-12-25 23:46:17 +0800

    add: images service module.

---
+++ fari-image/src/test/java/com/gomnitrix/fari_image/FariImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomnitrix.fari_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FariImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
